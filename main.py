c23a2281b44a522c768ba291de55cbd994941e9c
isPrabhpuran <prabh@autonomixsolutions.com>
2026-01-05 17:10:00 +0530

    learnt about models

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q" : q}
 
-# get -> kuch lene ke liye
-# post -> kuch dene ke liye
-# patch -> jo already pata hai usse badalne ke liye
-# delete -> delete karne ke liye
-# put -> update karne ke liye
-
-# / signifies the home page ka
-
-# decorators
-# error numbers
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
